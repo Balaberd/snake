8fc764fb53916be57d10fb009ed665d4eab34294
Balaberd <anenburg.alexandr@mail.ru>
2022-04-02 15:59:21 +0500

    логика готова,много лишнего кода,поработать с визуалом

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 //направления движения и текущее направление
-let topStep = [0, -16];
-let rightStep = [16, 0];
-let bottomStep = [0, 16];
-let leftStep = [-16, 0];
+const topStep = [0, -16];
+const rightStep = [16, 0];
+const bottomStep = [0, 16];
+const leftStep = [-16, 0];
 let currentDirection = topStep;
-let speed = 1000;
-
-/* текст на канвасе
-    context.fillStyle = "black";
-    context.font = 'bold 35px sans-serif';
-    context.strokeText("Вы проиграли", 15, 100);
-*/
+let speed = 500;
+let score = 0;
+let maxScore = 0;
 
 let snake = {
   //положение тела змейки
@@ -33,20 +29,28 @@ let snake = {
   },
   //обновление тела змейки
   refreshSnakeBodyPosition() {
+
     let newHeadPosition = [snake.bodyPosition[0][0] + currentDirection[0], snake.bodyPosition[0][1] + currentDirection[1]];
+      //обновление головы
       snake.bodyPosition.unshift(newHeadPosition);
+      //удалние хвоста если змейка не ест ягодку // переделать на if else
       if((berry.berryPosition[0] != snake.bodyPosition[0][0]) || (berry.berryPosition[1] != snake.bodyPosition[0][1])) snake.bodyPosition.pop();
-      if((berry.berryPosition[0] == snake.bodyPosition[0][0]) && (berry.berryPosition[1] == snake.bodyPosition[0][1])) berry.paintBerry();
+      //если змейка съедает ягодку
+      if((berry.berryPosition[0] == snake.bodyPosition[0][0]) && (berry.berryPosition[1] == snake.bodyPosition[0][1])) {
+        refreshScore();
+        berry.paintBerry();
+      }
+    newHeadPosition = undefined;
   },
   //проверка на поражение
   check() {
     for(let i = 1; i < snake.bodyPosition.length; i++) {
       //пересечение с телом (кроме головы)
-      if((snake.bodyPosition[0][0] == snake.bodyPosition[i][0]) && (snake.bodyPosition[0][1] == snake.bodyPosition[i][1])) console.log('LOSE');
+      if((snake.bodyPosition[0][0] == snake.bodyPosition[i][0]) && (snake.bodyPosition[0][1] == snake.bodyPosition[i][1])) loseGame();
     };
     //пересечение с полем
-    if((snake.bodyPosition[0][0] < 0) || (snake.bodyPosition[0][1] < 0)) console.log('LOSE');
-    if((snake.bodyPosition[0][0] > 272) || (snake.bodyPosition[0][1] > 272)) console.log('LOSE');
+    if((snake.bodyPosition[0][0] < 0) || (snake.bodyPosition[0][1] < 0)) loseGame();
+    if((snake.bodyPosition[0][0] > 272) || (snake.bodyPosition[0][1] > 272)) loseGame();
   },
   //ф-я анимации
   animation() {
@@ -57,6 +61,7 @@ let snake = {
     console.log('голова ' + snake.bodyPosition[0] + " berry " + berry.berryPosition);
   },
 };
+
 let berry = {
   berryPosition: [],
   //обновление положения ягодки
@@ -80,6 +85,8 @@ let berry = {
  
 };
 
+
+/* ---------------------------------------  НАПРАВЛЕНИЕ ДВИЖЕНИЯ -------------- */
 //управление - прослушать кнопку изменить направление
 document.addEventListener('keydown', (event) => {
   if(event.key == 'ArrowUp' && currentDirection != bottomStep) currentDirection = topStep;
@@ -91,30 +98,69 @@ document.addEventListener('keydown', (event) => {
 snake.paintSnake();
 berry.paintBerry();
 
-
-
+/* -------------------------------------- КНОПКА УПРАВЛЕНИЯ ------------------------------- */
+//управление игрой
 //сделать адаптивную кнопку Старт => пауза
-
 let superButton = document.querySelector('.super-button');
 superButton.addEventListener('click', startPlay);
-
 let play;
-
 //ф-я старт
-//изменить ф-ю кноки на ф-ю пауза
-//изменить текст кноки на старт
+//изменить ф-ю кноки на ф-ю пауза / изменить текст кноки на старт
 function startPlay() {
-  play = setInterval(snake.animation, 1000);
+  if(superButton.innerHTML == 'НАЧАТЬ CНАЧАЛА') {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    snake.paintSnake();
+    berry.paintBerry();
+    console.log('НАЧАТЬ СНАЧАЛА!!!')
+  };
+  play = setInterval(snake.animation, speed);
   superButton.removeEventListener('click', startPlay);
   superButton.addEventListener('click', pausePlay);
-  superButton.innerHTML = 'ПАУЗА'
+  superButton.innerHTML = 'ПАУЗА';
 };
-
-
 //ф-я пауза / обратно старту
 function pausePlay() {
   clearInterval(play);
   superButton.removeEventListener('click', pausePlay);
   superButton.addEventListener('click', startPlay);
-  superButton.innerHTML = 'СТАРТ'
+  superButton.innerHTML = 'СТАРТ';
 }
+
+/* -------------------------------- ОЧКИ ------------------------------------------- */
+
+let scoreNow = document.querySelector('.score__now');
+scoreNow.innerHTML = 'Результат: ' + score;
+
+let scoreMax = document.querySelector('.score__max');
+scoreMax.innerHTML = 'Лучший результат: ' + maxScore;
+
+
+function refreshScore() {
+  score++;
+  scoreNow.innerHTML = 'Результат: ' + score;
+}
+
+
+/*** -------------------------------- ПРОИГРЫШ ----------------------------------- */
+
+function loseGame() {
+
+  pausePlay();
+  superButton.innerHTML = 'НАЧАТЬ CНАЧАЛА';
+
+  snake.bodyPosition = [[128,128],[128,144],[128,160]];
+  currentDirection = topStep;
+  if(score > maxScore) maxScore = score;  
+  scoreMax.innerHTML = 'Лучший результат: ' + maxScore;
+  score = 0;
+  scoreNow.innerHTML = 'Результат: ' + score;
+
+  
+  context.fillStyle = "black";
+  context.font = 'bold 35px sans-serif';
+  
+  context.strokeText("Вы проиграли", 15, 100);
+
+}
+
+
